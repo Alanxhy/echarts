2c715837ca07b22a7a453e2925ce636acfd1fc81
Alanxhy <lanxin416@163.com>
2020-07-27 17:46:13 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <el-button>999</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home'
+
+}
+</script>
